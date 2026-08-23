808909228cc42dc2ac003d2894b5c9619e0b37c9
Lia <natalimail911@gmail.com>
2026-08-24 01:59:09 +0300

    components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,18 @@
+import css from "./App.module.css";
 // import { useState, useEffect } from "react";
 // import styles from "./App.module.css";
-// import CafeInfo from "../CafeInfo/CafeInfo";
+import CafeInfo from "../CafeInfo/CafeInfo";
 // import Notification from "../Notification/Notification";
 // import VoteOptions from "../VoteOptions/VoteOptions";
 // import VoteStats from "../VoteStats/VoteStats";
 
-import css from "./App.module.css";
-
 const App = () => {
-  return <div className={css.app}></div>;
+  return (
+    <div className={css.app}>
+      {" "}
+      <CafeInfo />
+    </div>
+  );
 };
 
 export default App;
