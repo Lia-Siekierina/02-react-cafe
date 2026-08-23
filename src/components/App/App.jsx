ca0c2fa6f99622922a24050e4f065b6793d2c69f
Lia <natalimail911@gmail.com>
2026-08-24 01:44:52 +0300

    main

--- src/components/App/App.jsx
+++
@@ -1,6 +0,0 @@
-import { useState, useEffect } from "react";
-import styles from "./App.module.css";
-import CafeInfo from "../CafeInfo/CafeInfo";
-import Notification from "../Notification/Notification";
-import VoteOptions from "../VoteOptions/VoteOptions";
-import VoteStats from "../VoteStats/VoteStats";
---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+// import { useState, useEffect } from "react";
+// import styles from "./App.module.css";
+// import CafeInfo from "../CafeInfo/CafeInfo";
+// import Notification from "../Notification/Notification";
+// import VoteOptions from "../VoteOptions/VoteOptions";
+// import VoteStats from "../VoteStats/VoteStats";
+
+import css from "./App.module.css";
+
+const App = () => {
+  return <div className={css.app}></div>;
+};
+
+export default App;
